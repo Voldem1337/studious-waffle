8af46eb516860568ca41e46a7b2febc08d4adc35
Voldem1337 <demjan.volynski@gmail.com>
2025-11-07 14:05:16 +0200

    loading bar broken

--- simcity_heart/main.py
+++ simcity_heart/main.py
@@ -23,45 +23,62 @@ class Loading_screen(arcade.View):
         self.sprite_list = arcade.SpriteList()
         self.sprite_list.append(self.bc)
         #parameters for progress-bar
-        self.progress =0
-        self.loading_speed = 30
+        self.progress = 10
+        self.loading_speed = 15
         self.bar_width = 400
-        self.bar_height = 400
-        self.bar_y = screen_height / 2
+        self.bar_height = 25
+        self.bar_y = screen_height / 2 -50
 
 
     #drawing sprite(image)
     def on_draw(self) -> None:
-        #creating picture
+        # creating picture
         self.clear()
         self.sprite_list.draw()
-        #creating loading bar
-        bar_x = window.width / 2
+
+        # creating loading bar
+        bar_x = window.width / 2 - 100
+
+        # рисуем рамку бара
         arcade.draw_lbwh_rectangle_outline(
-            bar_x,self.bar_y,
-            self.bar_width,self.bar_height,
-            arcade.color.WHITE,3
+            bar_x, self.bar_y,
+            self.bar_width, self.bar_height,
+            arcade.color.WHITE
         )
+
+        # вычисляем ширину заливки
         fill_width = (self.progress / 100) * (self.bar_width - 6)
-        #filling code
+
+        # вычисляем левый край и центр заливки, чтобы она росла слева направо
+        print(self)
+        left_edge_x = bar_x - (self.bar_width - 6) / 2
+        fill_center_x = left_edge_x + fill_width / 2
+
+        # filling code
         if fill_width > 0:
             arcade.draw_lbwh_rectangle_filled(
-                bar_x -(self.bar_width -6- fill_width)/2,
-            self.bar_y,fill_width,
-            self.bar_height -6,
-            arcade.color.GREEN)
-        #loading txt
-        precent_text =f"{int(self.progress)}%"
+                fill_center_x,
+                self.bar_y,
+                fill_width,
+                self.bar_height - 6,
+                arcade.color.WHITE
+            )
+
+        # loading txt
+        percent_text = f"{int(self.progress)}%"
         arcade.draw_text(
-            precent_text,
+            percent_text,
             bar_x - 20, self.bar_y - 10,
-            arcade.color.WHITE,14
+            arcade.color.WHITE, 14
         )
-    def on_update(self,delta_time: float):
-        if self.progress >= 100:
-            self.progress += self.loading_speed * delta_time
-        else:
-            self.progress = 100
+
+        #Should be when project is ready #self.progress = (загружено_файлов / общее_количество_файлов) * 100
+
+    # def on_update(self, delta_time: float):
+    #     if self.progress < 100:
+    #         self.progress += self.loading_speed * delta_time
+    #     else:
+    #         self.progress = 100
 
 
 
