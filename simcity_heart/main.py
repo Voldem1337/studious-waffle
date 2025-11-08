405bddf5b37cd226b70bcef4546cdf17e20cec67
Voldem1337 <demjan.volynski@gmail.com>
2025-11-08 20:34:19 +0200

    loading bar broken

--- simcity_heart/main.py
+++ simcity_heart/main.py
@@ -1,9 +1,26 @@
 import arcade
-
+import os
 
 # window = arcade.Window(fullscreen= True, title="SimCity Heart")
 window = arcade.Window( title="SimCity Heart", width=1280, height=800 )
 window.center_window()
+
+class MainView(arcade.View):
+    def __init__(self):
+        super().__init__()
+        self.bar_x = window.width / 2 - 100
+        self.bar_y = window.height / 2
+
+
+    def on_draw(self) -> None:
+        self.clear()
+        percent_text = f"Menuu"
+        arcade.draw_text(
+            percent_text,
+            self.bar_x + 100, self.bar_y + 50,
+            arcade.color.WHITE, 28
+        )
+
 
 
 class Loading_screen(arcade.View):
@@ -23,12 +40,19 @@ class Loading_screen(arcade.View):
         self.sprite_list = arcade.SpriteList()
         self.sprite_list.append(self.bc)
         #parameters for progress-bar
-        self.progress = 10
-        self.loading_speed = 15
+        self.progress = 0
+        self.loading_speed = 20
         self.bar_width = 400
         self.bar_height = 25
         self.bar_y = screen_height / 2 -50
 
+        #da
+        self.image_folder = "../assets/images"
+        self.files = os.listdir(self.image_folder)
+        self.total_files = len(self.files)
+        self.loaded_files = 0
+        self.progress = 0
+        self.textures = []
 
     #drawing sprite(image)
     def on_draw(self) -> None:
@@ -47,11 +71,11 @@ class Loading_screen(arcade.View):
         )
 
         # вычисляем ширину заливки
-        fill_width = (self.progress / 100) * (self.bar_width - 6)
+        fill_width = (self.progress / 100) * (self.bar_width -0)
 
         # вычисляем левый край и центр заливки, чтобы она росла слева направо
-        print(self)
-        left_edge_x = bar_x - (self.bar_width - 6) / 2
+
+        left_edge_x = bar_x - fill_width /2
         fill_center_x = left_edge_x + fill_width / 2
 
         # filling code
@@ -60,25 +84,42 @@ class Loading_screen(arcade.View):
                 fill_center_x,
                 self.bar_y,
                 fill_width,
-                self.bar_height - 6,
+                self.bar_height,
                 arcade.color.WHITE
             )
 
         # loading txt
-        percent_text = f"{int(self.progress)}%"
+        percent_text = f"Loading... {int(self.progress)}%"
         arcade.draw_text(
             percent_text,
-            bar_x - 20, self.bar_y - 10,
-            arcade.color.WHITE, 14
+            bar_x+100, self.bar_y+33 ,
+            arcade.color.WHITE, 28
         )
 
         #Should be when project is ready #self.progress = (загружено_файлов / общее_количество_файлов) * 100
 
-    # def on_update(self, delta_time: float):
-    #     if self.progress < 100:
-    #         self.progress += self.loading_speed * delta_time
+    def on_update(self, delta_time: float):
+        if self.progress < 100:
+            self.progress += self.loading_speed * delta_time
+        else:
+            self.progress = 100
+            main = MainView()
+            window.show_view(main)
+    # def on_update(self, delta_time):
+    #     # загружаем по одному файлу за кадр (для демонстрации)
+    #     if self.loaded_files < self.total_files:
+    #         file_name = self.files[self.loaded_files]
+    #         file_path = os.path.join(self.image_folder, file_name)
+    #         texture = arcade.load_texture(file_path)
+    #         self.textures.append(texture)
+    #         self.loaded_files += 1
+    #         # вычисляем процент
+    #         self.progress = (self.loaded_files / self.total_files) * 100
     #     else:
-    #         self.progress = 100
+    #         main = MainView()
+    #         window.show_view(main)
+    #         print("Все ресурсы загружены!")
+
 
 
 
